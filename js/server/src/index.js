1c4548f5924b6fd005e2eec4b8e14147a5431c5d
airglow923 <p.hyundeok76@gmail.com>
2021-02-06 16:29:32 +0800

    remove babel from server

--- js/server/src/index.js
+++ js/server/src/index.js
@@ -1,31 +1,29 @@
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import morgan from 'morgan';
 import db from './models';
 
+const currentDir = dirname(fileURLToPath(import.meta.url));
+const frontPath = resolve(currentDir, 'views/');
 const app = express();
 
 const corsOptions = {
   origin: 'http://localhost:8080',
 };
 
+app.use(express.static(frontPath));
+app.use(morgan('combined'));
 app.use(cors(corsOptions));
-
 app.use(bodyParser.json());
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
-db.sequelize
-  .sync({ force: true })
-  .then(() => {
-    console.log('Drop and resync');
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+db.sequelize.sync({ force: true });
 
 app.get('/', (req, res) => {
-  res.json({ message: 'GET HTTPS request' });
+  res.sendFile(resolve(frontPath, 'index.html'));
 });
 
 const PORT = process.env.PORT || 8080;
