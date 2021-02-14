d6e905aed676077dad6924485798158878174d47
airglow923 <p.hyundeok76@gmail.com>
2021-02-14 21:09:45 +0800

    edit configuration

---
+++ js/server/src/config/index.js
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ silent: true });
+
+export default {
+  port: process.env.PORT,
+  db: {
+    database: process.env.DATABASE,
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    options: {
+      host: process.env.HOST,
+      dialect: process.env.DIALECT,
+      pool_max: process.env.POOL_MAX,
+      pool_min: process.env.POOL_MIN,
+      pool_acqurie: process.env.POOL_ACQURIE,
+      pool_idle: process.env.POOL_IDLE,
+    },
+  },
+};
