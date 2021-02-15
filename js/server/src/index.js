c3e5a694329fb5c5e9253a47878624e1030c050f
airglow923 <p.hyundeok76@gmail.com>
2021-02-15 12:15:42 +0800

    edit web server

--- js/server/src/index.js
+++ js/server/src/index.js
@@ -1,33 +1,43 @@
 import { dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
 import express from 'express';
+import compression from 'compression';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import morgan from 'morgan';
+import config from './config';
 import db from './models';
+import users from './routes/users';
 
 const currentDir = dirname(fileURLToPath(import.meta.url));
 const frontPath = resolve(currentDir, 'views/');
-const app = express();
-
+const PORT = config.port || 8080;
 const corsOptions = {
   origin: 'http://localhost:8080',
 };
 
-app.use(express.static(frontPath));
-app.use(morgan('combined'));
-app.use(cors(corsOptions));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const main = async () => {
+  await db.sequelize
+    .authenticate()
+    .then(() => console.log('Database successfully connected.'));
 
-db.sequelize.sync({ force: true });
+  const app = express();
+  app.use(express.static(frontPath));
+  app.use(compression());
+  app.use(morgan('combined'));
+  app.use(cors(corsOptions));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(frontPath, 'index.html'));
-});
+  app.get('/', (req, res) => res.send(resolve(frontPath, 'index.html')));
 
-const PORT = process.env.PORT || 8080;
+  app.use('/users', users);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+  await db.sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}.`);
+    });
+  });
+};
+
+main().catch((err) => console.error(`${err}`));
