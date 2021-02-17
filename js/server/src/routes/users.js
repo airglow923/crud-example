9890623e6b0184d6ee7c6b85e9021488fcf98e2e
airglow923 <p.hyundeok76@gmail.com>
2021-02-17 22:48:18 +0800

    edit server router

--- js/server/src/routes/users.js
+++ js/server/src/routes/users.js
@@ -1,7 +1,10 @@
 import { Router } from 'express';
+import { register } from '../controllers/RegisterController';
+import ReigsterControllerPolicy from '../policies/RegisterControllerPolicy';
 
 const router = Router();
 
-router.get('/', (req, res) => res.send('users'));
+router.get('/', (req, res) => res.send('Hello this is /users'));
+router.post('/register', ReigsterControllerPolicy, register);
 
 export default router;
