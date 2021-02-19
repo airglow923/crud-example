a5f7396c391dd0d028b38dfef70ae759810f7c04
airglow923 <p.hyundeok76@gmail.com>
2021-02-20 00:18:43 +0800

    replae || with ??

--- js/server/src/config/index.js
+++ js/server/src/config/index.js
@@ -9,18 +9,18 @@ dotenvSafe.config({
 });
 
 export default {
-  port: process.env.PORT || 8080,
+  port: process.env.PORT ?? 8080,
   db: {
-    database: process.env.DATABASE || 'rest_api_example',
-    username: process.env.USERNAME || 'root',
-    password: process.env.PASSWORD || '',
+    database: process.env.DATABASE ?? 'rest_api_example',
+    username: process.env.USERNAME ?? 'root',
+    password: process.env.PASSWORD ?? '',
     options: {
-      host: process.env.HOST || 'localhost',
-      dialect: process.env.DIALECT || 'mysql',
-      pool_max: process.env.POOL_MAX || 5,
-      pool_min: process.env.POOL_MIN || 0,
-      pool_acqurie: process.env.POOL_ACQURIE || 30000,
-      pool_idle: process.env.POOL_IDLE || 10000,
+      host: process.env.HOST ?? 'localhost',
+      dialect: process.env.DIALECT ?? 'mysql',
+      pool_max: process.env.POOL_MAX ?? 5,
+      pool_min: process.env.POOL_MIN ?? 0,
+      pool_acqurie: process.env.POOL_ACQURIE ?? 30000,
+      pool_idle: process.env.POOL_IDLE ?? 10000,
       define: {
         freezeTableName: true,
       },
