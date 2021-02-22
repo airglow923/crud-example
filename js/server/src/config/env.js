fb5abc6aadbe0f4b5638093bd83aa88016f465ba
airglow923 <p.hyundeok76@gmail.com>
2021-02-22 17:53:47 +0800

    edit env directory

--- js/server/src/config/env.js
+++ js/server/src/config/env.js
@@ -1,11 +1,11 @@
 const resolveEnvPath = (mode) => {
   switch (mode) {
     case 'development':
-      return '.env.development';
+      return '../.env.development';
     case 'production':
-      return '.env.production';
+      return '../.env.production';
     case 'ci':
-      return '../../.env.ci';
+      return '../ci/.env.ci';
     default:
       return new Error(`Invalid Node mode ${mode}`);
   }
